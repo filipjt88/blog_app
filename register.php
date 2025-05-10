f429d3112ed894d914d324323e48dbca952fc639
filipjt88 <filip88ks@gmail.com>
2025-05-10 22:14:43 +0200

    register view

--- register.php
+++ register.php
@@ -1,21 +1,7 @@
 <?php include 'parts/top.php'; ?>
 <?php include 'parts/navbar.php'; ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 offset-3">
-            <h1>Register</h1>
-            <form action="register.php" method="POST">
-                <input type="text" name="first_name" class="form-control" placeholder="Please enter first name"><br>
-                <input type="text" name="last_name" class="form-control" placeholder="Please enter last name"><br>
-                <input type="email" name="email" class="form-control" placeholder="Please enter email"><br>
-                <input type="password" name="password" class="form-control" placeholder="Please enter the password"><br>
-                <input type="password" name="password_confirm" class="form-control" placeholder="Please enter the password repeat"><br>
-                <button type="submit" class="btn btn btn-success">Register</button>
-            </form>
-        </div>
-    </div>
-</div>
+
 
 
 <?php include 'parts/bottom.php'; ?>
