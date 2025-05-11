b2ae6b56402724f766255262153406345b17ebfe
filipjt88 <filip88ks@gmail.com>
2025-05-11 21:32:35 +0200

    include links

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,32 +1,22 @@
   <div class="container">
       <nav class="navbar navbar-expand-lg bg-light">
-          <a class="navbar-brand" href="#">Blog app</a>
+          <a class="navbar-brand" href="../index.php">Blog app</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
               <ul class="navbar-nav mx-auto">
                   <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#">Home page</a>
+                      <a class="nav-link active" aria-current="page" href="../index.php">Home page</a>
                   </li>
                   <li class="nav-item">
                       <a class="nav-link" href="#">Blog post</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="../views/register.view.php">Register</a>
+                      <a class="nav-link" href="views/register.view.php">Register</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="../views/login.view.php">Login</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Dropdown link
-                      </a>
-                      <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
-                      </ul>
+                      <a class="nav-link" href="././views/login.view.php">Login</a>
                   </li>
               </ul>
           </div>
