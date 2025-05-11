1c298259352fc2bb743414e9a91f67d4b505dcc2
filipjt88 <filip88ks@gmail.com>
2025-05-11 22:35:57 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 // Initialization for ES Users
 import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
-
 initMDB({ Dropdown, Collapse });
+
+// Validacija forme za registraciju
+
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    const username            = document.getElementById('username').value.trim();
+    const email               = document.getElementById('email').value.trim();
+    const password            = document.getElementById('password').value.trim();
+    const password_confirm    = document.getElementById('password_confirm').value.trim();
+    const error = document.getElementById('error');
+
+    let errorMsg = '';
+
+    if(!username || !email || !password || !password_confirm) {
+        errorMsg = 'Sva polja su obavezna!';
+    } else if(password.length < 6) {
+        errorMsg = 'Lozinka mora imati minimum 6 karaktera!';
+    } else if(password !== password_confirm) {
+        errorMsg = 'Lozinke se ne poklapaju!';
+    }
+
+    if(errorMsg) {
+        e.preventDefault();
+        error.textContent = errorMsg;
+    }
+
+
+
+});
--- views/register.view.php
+++ views/register.view.php
@@ -6,11 +6,11 @@
     <div class="row">
         <div class="col-md-6 offset-3 mt-5">
             <h1 class="text-center">Registracija</h1>
-            <form action="register.php" method="POST">
-                <input type="text" name="username" class="form-control" placeholder="Please enter username"><br>
-                <input type="email" name="email" class="form-control" placeholder="Please enter email"><br>
-                <input type="password" name="password" class="form-control" placeholder="Please enter the password"><br>
-                <input type="password" name="password_confirm" class="form-control" placeholder="Please enter the password repeat"><br>
+            <form id="registerForm" action="../register.php" method="POST" novalidate>
+                <input type="text" id="username" name="username" class="form-control" placeholder="Please enter username"><br>
+                <input type="email" id="email" name="email" class="form-control" placeholder="Please enter email"><br>
+                <input type="password" id="password" name="password" class="form-control" placeholder="Please enter the password"><br>
+                <input type="password" id="password_confirm" name="password_confirm" class="form-control" placeholder="Please enter the password repeat"><br>
                 <button type="submit" class="btn btn btn-success">Registruj se</button>
             </form>
         </div>
