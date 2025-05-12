ffe4f64f5fd054a066b96756ddea9c71098261e8
filipjt88 <filip88ks@gmail.com>
2025-05-12 22:37:10 +0200

    save

--- views/index.view.php
+++ views/index.view.php
@@ -1,15 +1,18 @@
 <div class="container">
-    <div class="d-flex justify-content-between align-items-center mb-4">
+            <div class="d-flex justify-content-between align-items-center mb-4">
         <h1 class="text-center mt-5">Blog postovi</h1>
         <section>
             <a href="create_blog_post.php" class="btn btn-sm btn-success me-2">+ Dodaj novi post</a>
-            <strong><?php htmlspecialchars($_SESSION['username']) ?></strong> |
+            <strong><?= htmlspecialchars($_SESSION['username']) ?></strong> |
             <a href="./logout.php" class="btn btn-sm btn-outline-secondary">Logout</a>
         </section>
     </div>
 </div>
 
-<?php if(count($posts) === 0):  ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 offset-1">
+                <?php if(count($posts) === 0):  ?>
     <h3 class="text-center mt-5">Jos uvek nema blog postova :-(</h3>
     <?php else: ?>
     <?php foreach ($posts as $post):  ?>
@@ -18,10 +21,13 @@
                 <h3 class="card-title"><?= htmlspecialchars($post['title']) ?></h3>
                 <p class="card-text"><?= nl2br(htmlspecialchars($post['content'])) ?></p>
                 <small class="text-muted">
-                    Autor: <?php htmlspecialchars($post['username']) ?? 'Nepoznat' ?> |
+                    Autor: <?= htmlspecialchars($post['username']) ?? 'Nepoznat' ?> |
                     Objavljeno : <?= date('d.m.Y. H:i', strtotime($post['created_at'])) ?>
                 </small>
             </div>
         </div>
     <?php endforeach; ?>
     <?php endif; ?>
+            </div>
+        </div>
+    </div>
