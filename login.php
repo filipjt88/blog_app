1551c3fb4473ec7bcb058515c7f26fafad04e935
filipjt88 <filip88ks@gmail.com>
2025-05-12 21:13:05 +0200

    login proccess

--- login.php
+++ login.php
@@ -1,6 +1,40 @@
+<?php session_start(); ?>
 <?php include 'parts/top.php'; ?>
 <?php include 'parts/navbar.php'; ?>
+<?php require_once('core/db.php'); ?>
 
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === "POST") {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    if(empty($email) || empty($password)) {
+        header("Location: login.php?error=" . urlencode("Sva polja su obavezna"));
+        exit;
+    }
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $stmt->execute(['email' => $email]);
+    $user = $stmt->fetch();
+
+    if(!$user || !password_verify($password, $user['password'])) {
+        header("Location: login.php?error=" .urlencode("Pogresan email ili lozinka!"));
+        exit;
+    }
+
+    // Cuvanje podatka usera u sesiji
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+    $_SESSION['role'] = $user['role'];
+
+    // Preusmerenje usera na pocetnu stranicu
+    header("Location: index.php");
+}
+
+
+
+?>
 
 
 
