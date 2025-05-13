f114b972612a24218bc171f2e2684db19dbd298d
filipjt88 <filip88ks@gmail.com>
2025-05-13 12:58:14 +0200

    save edit process_post

--- edit_post.php
+++ edit_post.php
@@ -1,4 +1,25 @@
 <?php 
+session_start();
+require_once 'core/db.php';
 
+if(!isset($_SESSION['user_id'])) {
+    header("Location: views/login.view.php");
+    exit;
+}
+
+if(!isset($_GET['id'])) {
+    header("Location:views/index.view.php");
+}
+
+$post_id = $_GET['id'];
+$stmt = $pdo->prepare("SELECT * FROM posts WHERE id = :id");
+$stmt->execute(['id' => $post_id]);
+$post = $stmt->fetch();
+
+// Proveri da li postoji user i da li je vlasnik
+if(!$post || $post['user_id'] != $_SESSION['user_id']) {
+    header("Location: views/index.view.php");
+    exit;
+}
 
 ?>
