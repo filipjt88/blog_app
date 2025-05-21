a112a829929f6e73c76c6f7c584ecbffeab86797
filipjt88 <filip88ks@gmail.com>
2025-05-21 11:14:51 +0200

    add css

--- parts/top.php
+++ parts/top.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 <body>
--- views/login.view.php
+++ views/login.view.php
@@ -3,14 +3,16 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-6 offset-2 mt-5">
-            <h1 class="text-center">Prijava</h1>
+        <div class="col-md-6 offset-3 mt-5">
             <form id="loginForm" action="../login.php" method="POST">
+            <h1 class="text-center">Prijava</h1>
+                <label for="email">Email:</label>
                 <input type="email" name="email" class="form-control" placeholder="Please enter email"><br>
+                <label for="email">Password:</label>
                 <input type="password" name="password" class="form-control" placeholder="Please enter the password"><br>
-                <button type="submit" class="btn btn btn-success">Login</button>
+                <button type="submit" class="form-control btn btn-success">Login</button>
             </form>
-            <a href="register.view.php">Nemate nalog?Registrujte se odmah!</a>
+            <a class="nav-link nav-link-form" href="register.view.php">Nemate nalog? Registrujte se!</a>
             <?php if(isset($_GET['error'])): ?>
                 <div class="alert alert-danger"><?php htmlspecialchars($_GET['error']) ?></div>
             <?php endif; ?>
