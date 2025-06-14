ecd03e2e75694171bb1410a3765660f6f9424d4f
filipjt88 <filip88ks@gmail.com>
2025-06-14 20:13:18 +0200

    update footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,6 +1,6 @@
 <?php require_once __DIR__ . '/../core/init.php'; ?>
 <div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center">
     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
       <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
       <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
@@ -13,7 +13,7 @@
 
 <div class="container">
   <footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <ul class="nav justify-content-center border-bottom mb-3">
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pocetna stranica</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Kategorije</a></li>
       <li class="nav-item"><a href="create_blog_post.php" class="nav-link px-2 text-muted">Novi post</a></li>
