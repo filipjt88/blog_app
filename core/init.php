0d083d58e93a963e93d505db87582162710e2991
filipjt88 <filip88ks@gmail.com>
2025-06-28 22:06:34 +0200

    init

--- core/init.php
+++ core/init.php
@@ -2,6 +2,14 @@
 // Init
 define('BASE_URL', '/blog_app/');
 
+if(isset($_SESSION['user_id'])) {
+    $stmt = $pdo->prepare("SELECT id, username, role FROM users WHERE id = ?");
+    $stmt->execute([$_SESSION['user_id']]);
+    $currentUser = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+$isAdmin = isset($currentUser['role']) && $currentUser['role'] === 'admin';
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
