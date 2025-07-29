5d8072793f8659f1ed1989059c866db0ca2a0be6
filipjt88 <filip88ks@gmail.com>
2025-07-29 23:12:23 +0200

    update register form

--- views/register.view.php
+++ views/register.view.php
@@ -5,19 +5,24 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 offset-3">
-            <form id="registerForm" action="../register.php" method="POST" novalidate>
-                <h1 class="text-center">Register</h1>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" class="form-control" placeholder="Please enter username..."><br>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" class="form-control" placeholder="Please enter email..."><br>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" class="form-control" placeholder="Please enter the password..."><br>
-                <label for="password_confirm">Password repeat:</label>
-                <input type="password" id="password_confirm" name="password_confirm" class="form-control" placeholder="Please enter the password repeat..."><br>
-                <button type="submit" class="btn btn btn-success form-control">Register</button>
-            </form>
-            <a href="login.view.php" class="nav-link nav-link-form">Imate nalog? Prijavite se!</a>
+           <form id="registerForm" action="../register.php" method="POST" novalidate class="login-form">
+                <h1>Register</h1>
+
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Please enter username..." required>
+
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Please enter email..." required>
+
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Please enter the password..." required>
+
+                <label for="password_confirm">Password repeat</label>
+                <input type="password" id="password_confirm" name="password_confirm" placeholder="Please enter the password repeat..." required>
+
+                <button type="submit">Register</button>
+                <a href="login.view.php" class="register-link">Imate nalog? Prijavite se!</a>
+                </form>
         </div>
     </div>
 </div>
